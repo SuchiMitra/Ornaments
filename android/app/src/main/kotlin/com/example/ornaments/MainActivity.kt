95ecd9ab742191dd6769edc53129e2c4825ae4c9
SuchiMitra <suchi5862m@gmail.com>
2022-01-13 00:40:31 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/ornaments/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ornaments
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ornaments/constants.dart';
+import 'package:ornaments/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ORNAMENTS',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
